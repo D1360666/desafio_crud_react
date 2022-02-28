c92014b1d4d47ed5ac9b17ef2254f0e669584bd5
Diego Gonzalez <degonzalez6@gmail.com>
2022-02-28 01:21:11 -0300

    Commit 3 - Se crean los componentes del paquete todo, donde controlamos formulario de mantenimiento(modificación y eliminación" Se crea store que controlará la carga de nuestra pantalla. Se respalda el App.js anterior y se sustituye por el App.js actual que llama a los nuevos archivos distribuidos de nuestro proyecto.

--- front/src/Components/events.js
+++ front/src/Components/events.js
@@ -1,11 +1,11 @@
-export const actionType =´{
-    LIST_CREATED: 'list.LIST_CREATED',
-    LIST_FINDED: 'list.LIST_FINDED',
-    LIST_DELETED: 'list.LIST_DELETED' 
+export const actionType = {
+    LIST_CREATED: "list.LIST_CREATED",
+    LIST_FINDED: "list.LIST_FINDED",
+    LIST_DELETED: "list.LIST_DELETED" 
 };
 
 export default{
-    save : { item } => ({type: actionType.LIST_CREATED, item}),
-    finded : {list} => ({type: actionType.LIST_FINDED, list}),
+    saved : (item) => ({type: actionType.LIST_CREATED, item}),
+    finded : (list) => ({type: actionType.LIST_FINDED, list}),
     deleted : (listId) => ({type: actionType.LIST_DELETED, listId}),
 };
